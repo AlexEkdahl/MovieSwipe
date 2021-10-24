c9bbb301e43c8795d90a0bf62378efd78e9fd094
Alex <alexanderekdahl@me.com>
2021-10-24 12:06:04 +0200

    design

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -1,14 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import connectMongoDB from './mongodb.js'
 import dotenv from 'dotenv'
 import users from '../api/routes/users.routes.js'
 import auth from '../api/routes/auth.routes.js'
 dotenv.config()
 
-const PORT = process.env.PORT || 4000
-const MONGODB_URL = process.env.MONGODB_URL
 const app = express()
 
 //middleware
@@ -25,16 +22,4 @@ app.use('*', (req, res, next) =>
   res.status(404).json({ error: 'page not found' })
 )
 
-//start server
-const start = async () => {
-  try {
-    await connectMongoDB(MONGODB_URL)
-    app.listen(PORT, () => {
-      console.log(`SERVER RUNNING ON PORT: ${PORT}`)
-    })
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-export default start
+export { app }
